da4cea46d2f20e9d941ccd0e0841220a79f8e86a
h2oche <h2oche22@gmail.com>
2019-01-30 12:43:48 +0900

    Create 1965.cpp

---
+++ lis/1965.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define MAX_N 1000000
+using namespace std;
+
+int buf[MAX_N] = { 0, };
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n; cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> buf[i];
+
+	vector<int> dp; dp.push_back(buf[0]);
+	for (int i = 1; i < n; i++) {
+		if (dp.back() < buf[i])
+			dp.push_back(buf[i]);
+		else {
+			auto it = lower_bound(dp.begin(), dp.end(), buf[i]);
+			*it = buf[i];
+		}
+	}
+
+	cout << dp.size();
+
+	return 0;
+}
